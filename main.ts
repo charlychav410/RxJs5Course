1e6f90856c9ede2cf83415eb908552324b9b28fa
charlychav410 <charlychav410@gmail.com>
2018-04-17 18:10:46 -0500

    rxjs

--- main.ts
+++ main.ts
@@ -1,1 +1,47 @@
-alert('Ok!');
+import { Observable } from "rxjs";
+let number = [1, 5, 10, 20];
+
+
+let source2=Observable.from(number).map(x=>x*2).filter(x=>x>=10);
+
+let source = Observable.create((observer)=>{
+/*number.forEach(element=>{
+    
+if (element===10){
+    observer.error('Ya valio!');
+}
+observer.next(element);
+});
+observer.complete();*/
+
+let index=0;
+let produceValue=()=>{
+observer.next(number[index++])
+if(index<number.length){
+setTimeout(()=>{
+    produceValue();
+},2000);
+
+}
+else{observer.complete();}
+}
+produceValue();
+}).map(x=>x*2).filter(x=>x>=10);
+
+
+source.subscribe(
+value=>{
+    console.log(`value: ${value}`);
+},
+error=>{console.log(`value: ${error}`);},
+()=>{console.log(`value: ${'complete'}`);}
+
+);
+source2.subscribe(
+    value=>{
+        console.log(`value: ${value}`);
+    },
+    error=>{console.log(`value: ${error}`);},
+    ()=>{console.log(`value: ${'complete'}`);}
+    
+    );
---
+++ main3.ts
@@ -0,0 +1,21 @@
+import {Observable, Observer} from "rxjs";
+let number = [1, 5, 10];
+let source = Observable.from(number);
+
+class MyObservable implements Observer<number> {
+    closed?: boolean;
+    next(value: number)
+    {console.log( `ValueOK: ${value} ` )};
+    error (err: any) {console.log( `Error: ${err} `)};
+    complete() {console.log( `Complete! `)};
+}
+
+source.subscribe(new MyObservable);
+source.subscribe(
+value=>{
+    console.log(`value: ${value}`);
+},
+error=>{console.log(`value: ${error}`);},
+()=>{console.log(`value: ${'complete'}`);}
+
+);
